d4a70b91146fec7c7fc6b40523388e2f8fdfbb25
COLDBEATZ <vbhrytsenko@gmail.com>
2025-03-14 17:06:47 +0200

    Fix regulation maintenance pages

--- src/app/components/admin/services/services.component.ts
+++ src/app/components/admin/services/services.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import { Button } from 'primeng/button';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from "@angular/core";
 import { MainComponent } from "../../internal/main/main.component";
 import { BreadcrumbComponent } from "../../internal/breadcrumb/breadcrumb.component";
 import { TranslateModule } from "@ngx-translate/core";
-import { TableModule } from "primeng/table";
 import { RegulationsMaintenance } from "../../../models/regulations-maintenance.model";
 import { CommonModule } from "@angular/common";
-import { ButtonGroup } from "primeng/buttongroup";
 import { PrimengDialogServiceComponent } from "./primeng-dialog-service/primeng-dialog-service.component";
-import { IconField } from "primeng/iconfield";
-import { InputIcon } from "primeng/inputicon";
-import { InputText } from "primeng/inputtext";
-import { Toast } from "primeng/toast";
 import { RegulationsMaintenanceService } from "../../../services/api/regulations-maintenance.service";
 import { MaintenanceTableComponent } from "../../shared/maintenance/maintenance-table.component";
+import { ConfirmDialog } from "primeng/confirmdialog";
+import { AlertComponent } from "../../internal/alert/alert.component";
+import { LeftPanelComponent } from "../../shared/left-panel/left-panel.component";
+import { MenuItem } from "primeng/api";
+import { BootstrapButtonComponent } from "../../shared/button/bootstrap-button.component";
+
+enum SuccessType {
+	MAINTENANCE_SAVED,
+	MAINTENANCE_DELETED
+}
 
 @Component({
 	selector: 'services-root',
@@ -25,16 +28,31 @@ import { MaintenanceTableComponent } from "../../shared/maintenance/maintenance-
 		MainComponent,
 		BreadcrumbComponent,
 		TranslateModule,
-		MaintenanceTableComponent
+		MaintenanceTableComponent,
+		PrimengDialogServiceComponent,
+		AlertComponent,
+		LeftPanelComponent,
+		BootstrapButtonComponent,
+		ConfirmDialog
 	],
 	standalone: true
 })
 export class ServicesComponent implements OnInit {
 
+	@ViewChild('maintenanceTableComponent', { static: false })
+	maintenanceTableComponent!: MaintenanceTableComponent;
+
+	@ViewChild(PrimengDialogServiceComponent) modalComponent!: PrimengDialogServiceComponent;
+
+	protected readonly SuccessType = SuccessType;
+	successType: SuccessType | null = null;
+
 	/**
 	 * Список всіх об'єктів регламентного обслуговування
 	 */
 	regulationsMaintenance: RegulationsMaintenance[] = [];
+
+	menuItems: MenuItem[] = [];
 
 	constructor(private regulationsMaintenanceService: RegulationsMaintenanceService) {
 
@@ -46,5 +64,31 @@ export class ServicesComponent implements OnInit {
 				this.regulationsMaintenance = regulationsMaintenance;
 			}
 		});
+	}
+
+	onClickEditMaintenance(maintenance: RegulationsMaintenance | null) {
+		if (this.modalComponent) {
+			this.modalComponent.maintenance = maintenance;
+			this.modalComponent.openModal();
+		}
+	}
+
+	handleMaintenanceDeleted(maintenance: RegulationsMaintenance) {
+		console.log(123);
+		this.regulationsMaintenanceService.deleteRegulationsMaintenance(maintenance).subscribe({
+			next: () => {
+				this.maintenanceTableComponent.handleMaintenanceDeleted(maintenance);
+
+				this.successType = SuccessType.MAINTENANCE_DELETED;
+				window.scroll({top: 0, left: 0, behavior: 'smooth'});
+			}
+		});
+	}
+
+	handleMaintenanceSaved(maintenance: RegulationsMaintenance) {
+		this.maintenanceTableComponent.handleMaintenanceSaved(maintenance);
+
+		this.successType = SuccessType.MAINTENANCE_SAVED;
+		window.scroll({top: 0, left: 0, behavior: 'smooth'});
 	}
 }
